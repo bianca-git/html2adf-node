cfcbf724563d58577877d944b926731d8eb71e7d
Bianca W <69695379+bianca-git@users.noreply.github.com>
2023-12-19 11:38:06 +1100

    Update package versions and add validator library

--- src/parseitem.js
+++ src/parseitem.js
@@ -28,6 +28,7 @@ import { JSDOM } from 'jsdom';
 import { defaultSchema } from '@atlaskit/adf-schema/dist/cjs/schema/default-schema.js';
 import { JSONTransformer } from '@atlaskit/editor-json-transformer';
 import { JIRATransformer } from '@atlaskit/editor-jira-transformer';
+import validator from 'validator';
 
 // Initialize the transformers
 const jiraTransformer = new JIRATransformer(defaultSchema);
@@ -38,19 +39,25 @@ const adfTransformer = new JSONTransformer();
  * @param {string} html - The HTML string to be converted.
  */
 function parseitem(html) {
-  html = html.replaceAll(/\n+|\\n+|  +/gm, '')
-  // Simulate browser environment
-  const dom = new JSDOM('<!doctype html><html><body></body></html>');
-  global.window = dom.window;
-  global.DOMParser = window.DOMParser;
-  global.Node = dom.window.Node;
-  global.HTMLElement = dom.window.HTMLElement; 
-  // Convert HTML to ADF
-  const pmNode = jiraTransformer.parse(html);
-  // Convert ADF to JSON
-  const adfJson = adfTransformer.encode(pmNode);
-  // Log the result
-  return JSON.stringify(adfJson)
+  try {
+    html = validator.escape(html)
+    html = html.replaceAll(/\n+|\\n+/gm, '').replaceAll(/ style=''/gm, '').replaceAll(/“+|”+|’+/gm, `'`)
+    // Simulate browser environment
+    const dom = new JSDOM('<!doctype html><html><body></body></html>');
+    global.window = dom.window;
+    global.DOMParser = window.DOMParser;
+    global.Node = dom.window.Node;
+    global.HTMLElement = dom.window.HTMLElement;
+    // Convert HTML to ADF
+    const pmNode = jiraTransformer.parse(html);
+    // Convert ADF to JSON
+    const adfJson = adfTransformer.encode(pmNode);
+    // Log the result
+    return JSON.stringify(adfJson)
+  }
+  catch (error) {
+    return { err: error, inputHTML: html }
+  }
 }
 
 export default parseitem
